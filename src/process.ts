74db9b8e0bfcc3da65757135b000f5badc1e512b
Elder Vivot <eldervivot@gmail.com>
2023-05-09 21:45:20 -0300

    feat: implementing to convert data access pref gyn to save in database

---
+++ src/process.ts
@@ -0,0 +1,70 @@
+import 'dotenv/config'
+import xlsx from 'xlsx'
+import path from 'path'
+import axios from 'axios'
+
+const API_HOST = process.env.API_HOST || ''
+const TENANT = process.env.TENANT || ''
+
+interface IDataPlanilha {
+    CPF: string
+    Nome: string
+    Senha: string
+}
+
+interface IDataToSave {
+    cpf: string
+    name: string
+    password: string
+}
+
+function readExcel(): IDataToSave[] {
+    const listAccess: IDataToSave[] = []
+
+    const pathFile = path.resolve('./data/planilha_modelo_prefeitura.xlsx')
+    const workbook = xlsx.readFile(pathFile)
+    const sheetNameList = workbook.SheetNames
+    const sheetToJson: Array<IDataPlanilha> = xlsx.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]])
+
+    for (const acesso of sheetToJson) {
+        const cpf = acesso.CPF.toString().normalize('NFD').replace(/[^0-9]/g, '')
+        const name = acesso.Nome.toString().normalize('NFD').replace(/[^a-zA-Z0-9/ ]/g, '')
+        const password = acesso.Senha.toString()
+        listAccess.push({
+            cpf, name, password
+        })
+    }
+
+    return listAccess
+}
+
+async function saveDatabase(dataToSave: IDataToSave[]) {
+    for (const access of dataToSave) {
+        try {
+            await axios.post<any>(API_HOST,
+                {
+                    idTypeAccessPortals: "6a009e00-47b0-4e45-a28f-87a3481b2060",
+                    nameAccess: access.name,
+                    login: access.cpf,
+                    password: access.password,
+                    status: "ACTIVE"
+                },
+                {
+                    headers: {
+                        tenant: TENANT
+                    }
+                }
+            )
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+
+function processMain() {
+    const listAccess = readExcel()
+    saveDatabase(listAccess)
+}
+
+processMain()
+
